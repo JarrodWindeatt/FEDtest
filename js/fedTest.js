370695a2105508094052b9e65ab8a81f2fc54f91
Jarrod Windeatt <54828664+JarrodWindeatt@users.noreply.github.com>
2019-09-03 13:59:02 +1000

    Add files via upload

---
+++ js/fedTest.js
@@ -0,0 +1,14 @@
+function clickImage1() {
+    console.log("Answer your body - Image 1: clicked");
+    window.open("html/Image1.htm", "", "width=480,height=750");
+}
+
+function clickImage2() {
+    console.log("Answer your body - Image 2: clicked");
+    window.open("html/Image2.htm", "", "width=480,height=363");
+}
+
+function clickImage3() {
+    console.log("Answer your body - Image 3: clicked");
+    window.open("html/Image3.htm", "", "width=480,height=363");
+} 
